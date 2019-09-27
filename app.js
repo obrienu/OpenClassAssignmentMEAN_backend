23c436da3a2c146cbc91964862f61d3ba878aef7
obrienu <obrienules@gmail.com>
2019-09-27 13:08:53 +0100

    add the delete route to app.js

--- app.js
+++ app.js
@@ -109,4 +109,18 @@ app.put('/api/recipes/:id', (req, res, next) => {
     });
 });
 
+app.delete('/api/recipes/:id', (req, res, next) => {
+  Recipe.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      });
+    });
+});
+
 module.exports = app;
